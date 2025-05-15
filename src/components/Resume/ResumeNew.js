28fc5967a92c8c1ab06eb251959b7e76b6894496
udaybhookya <budaychandra2014@gmail.com>
2025-05-15 09:08:34 +0530

    Deploy updated site

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Uday-Chandra-Bhookya-J25-v2.pdf";
+import pdf from "../../Assets/Uday_Resume_M_25.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
