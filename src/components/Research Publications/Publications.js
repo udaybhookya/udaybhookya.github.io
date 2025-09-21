e6f19b735e5294f598eda39148a5a244749f1a9c
udaybhookya <budaychandra2014@gmail.com>
2025-09-21 17:03:59 +0530

    updated publications

--- src/components/Research Publications/Publications.js
+++ src/components/Research Publications/Publications.js
@@ -18,12 +18,29 @@ function Publications() {
         {/* --- Publications Section --- */}
         <h2 className="purple mb-4">📄 Publications</h2>
         <Row className="justify-content-center g-4">
+
+          <Col xs={12} md={10} className="project-card">
+            <PublicationsCard
+              title="Data Stories From Insights for IT Operations"
+              description= "IEEE International Conference on Big Data (Big Data), 2025"
+              abstract="With increasing focus on observability and analytics, a large volume of data-driven insights gets generated. However, the users face challenges in sifting through these insights to identify the relevant ones. Furthermore, looking at these insights in isolation fails to provide a comprehensive understanding of the situation. This paper addresses this problem of insight fatigue by recommending the relevant insights to the user and generating chains of relevant insights, with a specific focus on the insights generated to manage IT operations. We demonstrate the effectiveness of our approach with quantitative and qualitative experimental evaluation and a real-world case study. [Under Review]"
+            />
+          </Col>
+
           <Col xs={12} md={10} className="project-card">
             <PublicationsCard
               title="Addressing Spend Leakage and Optimization of Cloud Costs"
               description="IEEE International Conference on Big Data (Big Data), 2024, Washington DC, USA."
               abstract="Spend leakage in a cloud estate manifests in many forms and requires a careful analysis of various metrics. We present a suite of features to detect such behavior and provide recommendations for optimization. We apply the proposed solution in practice by presenting a real-world case-study. Through this case-study we demonstrate how many cases of spend leakage go unnoticed by simple handles to manage cloud estate and how the solutions proposed in the paper can enable the business with better foresight and control on the cloud costs."
               link="https://ieeexplore.ieee.org/abstract/document/10825120"
+            />
+          </Col>
+
+          <Col xs={12} md={10} className="project-card">
+            <PublicationsCard
+              title="Cloud Cost Optimization with Data Storytelling"
+              description="IEEE ICDMW 2025, USA"
+              abstract="Cloud cost optimization is one of the top priorities for most enterprises. However, given the scale and complexity, detecting cloud sprawl and spend leakage and recommending opportunities for optimization presents several challenges. Furthermore, cloud architects face insight fatigue with the sheer volume of these data-driven insights. This leads to significant resistance in the adoption of analytics-driven solutions for cloud cost optimization. In this paper, we address this challenge by presenting analytics-driven solutions to detect cloud inefficiencies and to generate actionable recommendations for addressing these inefficiencies. Furthermore, we address the problem of insight fatigue by presenting novel data story solutions to help users identify the insights of their interest. We validate our approach through a real-world running example of a leading IT organization."
             />
           </Col>
 
@@ -40,18 +57,11 @@ function Publications() {
             <PublicationsCard
               title="Addressing Insight Fatigue with Insight Summarization"
               description="COMSNETS 2025 – International Communication Systems and Networks Workshops."
-              abstract="Introduces a summarization framework to reduce cognitive overload caused by frequent, overlapping AI insights."
+              abstract="In the era of data-driven decisions-making organizations are increasingly challenged by insight fatigue, where a large volume insights overwhelm decision makers, hindering effective decision making. The paper addresses this issue by proposing for `Insight Summarization`. The Solution leverages an 'insight graph' and frequent pattern mining to recursively group similar insights, select the right cohorts if insights, and generate concise and interpretable summaries. We evaluate the effectiveness of our approach by generating summaries for insights generated on real-world dataset."
               link="https://ieeexplore.ieee.org/document/your-paper-link"
             />
           </Col>
 
-          <Col xs={12} md={10} className="project-card">
-            <PublicationsCard
-              title="Addressing AIOps Data Fatigue with Insight Chains"
-              description="ECML PKDD 2025 – European Conference on Machine Learning and Principles and Practice of Knowledge Discovery in Databases.  [Under Review]"
-              abstract="Presents a graph-based method to connect operational events into coherent, actionable insight chains."
-            />
-          </Col>
         </Row>
 
         {/* --- Patents Section --- */}
